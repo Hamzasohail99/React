c77f0561cc15f7b04020c46b74e54f6e3cea471c
Hamza Sohail <102827735+Hamzasohail99@users.noreply.github.com>
2022-10-05 12:10:27 +0000

    arrow function

---
+++ es6-arrow-function.js
@@ -0,0 +1,22 @@
+function square(x){
+    return x * x;
+}
+
+console.log(square(8));
+
+// const squarearrow = (x) => {
+//     return x * x;
+// }
+
+const squarearrow = (x) => x * x;
+
+console.log(squarearrow(9));
+
+// const getfirstname = (fullname) => {
+//     return fullname.split(' ')[0];
+// }
+
+const getfirstname = (fullname) => fullname.split(' ')[0];
+
+console.log(getfirstname('mike smith'));
+
