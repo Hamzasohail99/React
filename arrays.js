e3838479c2361d183aae4c02c48a059918ca13f9
Hamza Sohail <102827735+Hamzasohail99@users.noreply.github.com>
2022-10-06 15:55:23 +0000

    making arrays

---
+++ arrays.js
@@ -0,0 +1,87 @@
+
+
+console.log('app.js is running');
+
+const app = {
+    title: 'some title',
+    subtitle: 'this is my subtitle',
+    options: []
+    
+};
+
+const onformsubmit = (e) => {
+    e.preventDefault();
+    console.log('form submit');
+    const option = e.target.elements.option.value;
+    if(option){
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        render();
+    }
+};
+
+// const template = (
+//     <div>
+//         <h1>{app.title}</h1> 
+//         {app.subtitle && <p>{app.subtitle}</p>}
+//         <p>{app.options.length > 0 ? 'here are your options' : 'no option'}</p>
+//         <p>{app.options.length}</p>
+//         <ol>
+//             <li>item one</li>
+//             <li>item two</li>
+//         </ol>
+//         <h1>balley balley</h1>
+//         <form onSubmit = {onformsubmit}>
+//             <input type = "text" name = "option"></input>
+//             <button>add option</button>
+//         </form>
+//     </div>
+// );
+
+const onremoveall = () => {
+    app.options = [];
+    render();
+};
+
+const onmakedecision = () => {
+    const randomnum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomnum];
+    alert(option);
+};
+
+const appRoot = document.getElementById('app');
+// ReactDOM.render(template, appRoot);
+
+const numbers = [55, 101, 1000];
+
+const render = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1> 
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'here are your options' : 'no option'}</p>
+            <p>{app.options.length}</p>
+            <button disabled = {app.options.length == 0} onClick = {onmakedecision}>what should i do?</button>
+            <button onClick = {onremoveall} >remove all</button>
+            {
+                // numbers.map((number) => {
+                //     return <p key={number}>number: {number}</p>;
+                // })    
+            }
+            <ol>
+                {
+                    app.options.map((option) =>{
+                        return <li key={option}>{option}</li>
+                    })
+                }
+            </ol>
+            <h1>balley balley</h1>
+            <form onSubmit = {onformsubmit}>
+                <input type = "text" name = "option"></input>
+                <button>add option</button>
+            </form>
+        </div>
+    );
+    ReactDOM.render(template, appRoot);
+};
+render();
