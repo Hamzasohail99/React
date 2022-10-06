43265c864150c5408cb2caac00f59f1f5f4b8b6f
Hamza Sohail <102827735+Hamzasohail99@users.noreply.github.com>
2022-10-06 15:42:15 +0000

    arrays in jsx

--- app.js
+++ app.js
@@ -43,8 +43,15 @@ const onremoveall = () => {
     render();
 };
 
+const onmakedecision = () => {
+    const randomnum = Math.random() * app.options.length;
+    console.log(randomnum);
+};
+
 const appRoot = document.getElementById('app');
 // ReactDOM.render(template, appRoot);
+
+const numbers = [55, 101, 1000];
 
 const render = () => {
     const template = (
@@ -53,10 +60,19 @@ const render = () => {
             {app.subtitle && <p>{app.subtitle}</p>}
             <p>{app.options.length > 0 ? 'here are your options' : 'no option'}</p>
             <p>{app.options.length}</p>
+            <button onClick = {onmakedecision}>what should i do?</button>
             <button onClick = {onremoveall} >remove all</button>
+            {
+                // numbers.map((number) => {
+                //     return <p key={number}>number: {number}</p>;
+                // })    
+            }
             <ol>
-                <li>item one</li>
-                <li>item two</li>
+                {
+                    app.options.map((option) =>{
+                        return <li key={option}>{option}</li>
+                    })
+                }
             </ol>
             <h1>balley balley</h1>
             <form onSubmit = {onformsubmit}>
